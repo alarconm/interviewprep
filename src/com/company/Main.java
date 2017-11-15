9f2f6de18cb3ec3682b489a29ac3ba013e643952
Mike <malarconii@yahoo.com>
2017-11-14 17:05:39 -0800

    completed fizzbuzz

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+import com.company.FizzBuzz;
+
+public class Main {
+
+    static int n = 100;
+
+    public static void main(String[] args) {
+	// write your code here
+        FizzBuzz.fizzBuzz(n);
+      }
+
+
+}
